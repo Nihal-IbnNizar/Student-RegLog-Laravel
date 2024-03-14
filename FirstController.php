1b12e06dbdd0e6ba93b4b2158613a06b8ede04e9
MUHAMMED NIHAL <142206176+Nihal-IbnNizar@users.noreply.github.com>
2024-03-14 11:31:49 +0530

    Add files via upload

---
+++ FirstController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FirstController extends Controller
+{
+    function loginfunction(){
+        return view('login');
+    }
+
+    function registerfunction(){
+        return view('register');
+    }
+}
